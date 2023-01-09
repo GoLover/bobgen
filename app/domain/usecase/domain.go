c191a8c5a9bb73b7cf2848a167640da0ae78d66b
Jim Mim <latebozorg@gmail.com>
2023-01-09 16:35:50 +0330

    [ADD] write content in generated files

--- app/domain/usecase/domain.go
+++ app/domain/usecase/domain.go
@@ -1,9 +1,11 @@
 package usecase
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
 
+	"github.com/dave/jennifer/jen"
 	"github.com/golover/bobgen/domain"
 )
 
@@ -16,9 +18,20 @@ func NewDomainUsecase(dr domain.GenericRepository) DomainUsecase {
 }
 
 func (du DomainUsecase) CreateDomain(dn string) error {
-	du.AddDURToDomain(dn, dn)
 	dn = correctLetterAndNumber(dn)
 	err := du.dr.CreateFile(dn+".go", "domain/")
+	err = du.dr.WriteFile(dn+".go", "domain/", []byte("package domain\n"))
+	err = du.dr.CreateFile(dn+".go", "app/"+dn+"/delivery/")
+	err = du.dr.WriteFile(dn+".go", "app/"+dn+"/delivery/", []byte("package delivery\n"))
+	err = du.dr.CreateFile(dn+".go", "app/"+dn+"/repository/")
+	err = du.dr.WriteFile(dn+".go", "app/"+dn+"/repository/", []byte("package repository\n"))
+	err = du.dr.CreateFile(dn+".go", "app/"+dn+"/usecase/")
+	err = du.dr.WriteFile(dn+".go", "app/"+dn+"/usecase/", []byte("package usecase\n"))
+	du.AddDURToDomain(dn, dn)
+	return err
+}
+func (du DomainUsecase) addToDomain(dn string, content []byte) error {
+	err := du.dr.WriteFile(dn+".go", "domain/", content)
 	return err
 }
 func (du DomainUsecase) AddDURToDomain(dn, un string) error {
@@ -27,25 +40,31 @@ func (du DomainUsecase) AddDURToDomain(dn, un string) error {
 	err = du.AddUsecaseToDomain(dn, un)
 	return err
 }
-func (du DomainUsecase) AddDeliveryToDomain(dn, un string) error {
+func (du DomainUsecase) AddDeliveryToDomain(dn, deln string) error {
 	dn = correctLetterAndNumber(dn)
-	un = correctLetterAndNumber(un)
-	return du.dr.CreateFile(un+".go", "app/"+dn+"/delivery/")
+	deln = correctLetterAndNumber(deln)
+	err := du.dr.WriteFile(deln+".go", "app/"+dn+"/delivery/", []byte(fmt.Sprintf("%#v\n", jen.Type().Id(deln+"Delivery").Struct())))
+	err = du.addToDomain(dn, []byte(fmt.Sprintf("%#v\n", jen.Type().Id(deln+"Delivery").Interface())))
+	return err
 }
 func (du DomainUsecase) AddUsecaseToDomain(dn, un string) error {
 	dn = correctLetterAndNumber(dn)
 	un = correctLetterAndNumber(un)
-	return du.dr.CreateFile(un+".go", "app/"+dn+"/usecase/")
+	err := du.dr.WriteFile(un+".go", "app/"+dn+"/usecase/", []byte(fmt.Sprintf("%#v\n", jen.Type().Id(un+"Usecase").Struct())))
+	err = du.addToDomain(dn, []byte(fmt.Sprintf("%#v\n", jen.Type().Id(un+"Usecase").Interface())))
+	return err
 }
-func (du DomainUsecase) AddRepositoryToDomain(dn, un string) error {
+func (du DomainUsecase) AddRepositoryToDomain(dn, rn string) error {
 	dn = correctLetterAndNumber(dn)
-	un = correctLetterAndNumber(un)
-	return du.dr.CreateFile(un+".go", "app/"+dn+"/repository/")
+	rn = correctLetterAndNumber(rn)
+	err := du.dr.WriteFile(rn+".go", "app/"+dn+"/repository/", []byte(fmt.Sprintf("%#v\n", jen.Type().Id(rn+"Repository").Struct())))
+	err = du.addToDomain(dn, []byte(fmt.Sprintf("%#v\n", jen.Type().Id(rn+"Repository").Interface())))
+	return err
 }
 func correctLetterAndNumber(dn string) string {
 	dn = strings.TrimSpace(dn)
 	var correctedStr string
-	makeItUpper := false
+	makeItUpper := true
 	for _, k := range dn {
 		b := k
 		if !unicode.IsLetter(b) && !unicode.IsNumber(b) {
